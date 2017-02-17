0b440a81c908960d70f458233a48e6e9d4355428
Myeong Kim <mkim@inhance.com>
2017-02-17 11:06:56 -0800

    'tap' event added

--- src/INHANCE_singleclick.jquery.js
+++ src/INHANCE_singleclick.jquery.js
@@ -15,21 +15,34 @@
 		events = events || 'click';
 		target = target || '';
 		delay = delay || 300;
+
+		var tappedInTime = false;
 		
 		return this.each(function () {
+			
+			events = events.replace('tap', 'touchstart touchend');
+			
 			$(this).on(events.split(' ').join('.sclick '), target, function (evt) {
-				var timeSaved = $(this).data('whenSingleClicked');
 				var timeCurr = new Date().getTime();
+				if (evt.type == 'touchstart') {
+					$(this).data('whenTouched', timeCurr);
+				}
+				else if (evt.type == 'touchend') {
+					var timeTouchSaved = $(this).data('whenTouched');
+					if (timeTouchSaved && (timeCurr - timeTouchSaved < delay)) tappedInTime == true;
+				}
+				var timeSaved = $(this).data('whenSingleClicked');
 				var timeDiff = timeCurr - timeSaved;
-				if (timeSaved && timeDiff < delay) {
+				if ((timeSaved && timeDiff < delay) || ((evt.type == 'touchstart' || evt.type == 'touchend') && !tappedInTime)) {
 					evt.stopImmediatePropagation();
 					return;
 				}
 				
 				$(this).data('whenSingleClicked', timeCurr);
 
+				tappedInTime = false;
+
 				if (typeof handler == 'function') handler.call(this, evt);
-				
 			});
 		});
 	};
