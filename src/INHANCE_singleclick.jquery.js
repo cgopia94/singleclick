d69565c3535a8cd976f6d2c9962745405cedca2e
Myeong Kim <mkim@inhance.com>
2017-02-20 09:18:54 -0800

    refactorized to make use of $.event.special

--- src/INHANCE_singleclick.jquery.js
+++ src/INHANCE_singleclick.jquery.js
@@ -3,47 +3,57 @@
 // date: 02/17/2017
 // Author: Myeong Kim
 // Example:
-// $('#theButton').sclick('click', function (evt) {...});
+// $('#theButton').on('sclick', ....);
 
 (function ($) {
-	'use strict';
-	$.fn.sclick = function(events, target, handler, delay) {
-		if (typeof target == 'function') {
-			delay = handler;
-			handler = target;
-		}
-		events = events || 'click';
-		target = target || '';
-		delay = delay || 300;
+  'use strict';
+  var events = 'click.sclick touchstart.sclick touchend.sclick';
+  var delay = 300;
 
-		var tappedInTime = false;
-		
-		return this.each(function () {
-			
-			events = events.replace('tap', 'touchstart touchend');
-			
-			$(this).on(events.split(' ').join('.sclick '), target, function (evt) {
-				var timeCurr = new Date().getTime();
-				if (evt.type == 'touchstart') {
-					$(this).data('whenTouched', timeCurr);
-				}
-				else if (evt.type == 'touchend') {
-					var timeTouchSaved = $(this).data('whenTouched');
-					if (timeTouchSaved && (timeCurr - timeTouchSaved < delay)) tappedInTime == true;
-				}
-				var timeSaved = $(this).data('whenSingleClicked');
-				var timeDiff = timeCurr - timeSaved;
-				if ((timeSaved && timeDiff < delay) || ((evt.type == 'touchstart' || evt.type == 'touchend') && !tappedInTime)) {
-					evt.stopImmediatePropagation();
-					return;
-				}
-				
-				$(this).data('whenSingleClicked', timeCurr);
+  $.event.special.sclick = {
+    setup: function () {
+      $(this).on(events, function (evt) {
+        var tappedInTime = false;
+        var timeCurr = new Date().getTime();
+        if (evt.type == 'touchstart') {
+          $(this).data('whenTouched', timeCurr);
+        }
+        else if (evt.type == 'touchend') {
+          var timeTouchSaved = $(this).data('whenTouched');
+          if (timeTouchSaved && (timeCurr - timeTouchSaved < delay * 2)) {
+            tappedInTime = true;
+          }
+        }
+        var timeSaved = $(this).data('whenSingleClicked');
+        var timeDiff = timeCurr - timeSaved;
+        var trigger = false;
+        if (evt.type == 'touchstart' || evt.type == 'touchend') {
+          // alert('hi3' + tappedInTime);
+          if (tappedInTime && (!timeSaved || timeSaved && timeDiff > delay)) {
+            trigger = true;
+          }
+        }
+        else if (!timeSaved || timeSaved && timeDiff > delay) {
+          trigger = true;
+        } 
+        
+        if (trigger) {
+          var originalType = evt.type;
+          evt.type = 'sclick';
 
-				tappedInTime = false;
+          $.event.dispatch.call(this, evt);
+          evt.type = originalType;
 
-				if (typeof handler == 'function') handler.call(this, evt);
-			});
-		});
-	};
+
+        }
+
+        if (evt.type == 'click' || evt.type == 'touchend') $(this).data('whenSingleClicked', timeCurr);
+
+        tappedInTime = false;
+      });
+    },
+    remove: function () {
+      $(this).off(events);
+    }
+  };
 }(jQuery));
